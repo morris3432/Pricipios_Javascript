c2267d97fbecd81b011ac99c592c8a5eb597fc6c
Cristian Yancis <bernardoyancis@gmail.com>
2024-08-31 10:47:00 -0600

    guardando cambios

---
+++ cocatenar/script.js
@@ -0,0 +1,13 @@
+let nom='Maria'
+let edad=18
+
+document.write(nom+' tiene '+edad+' años')// manera 1 de concaatenar
+document.write('<br>')//salto de linea
+document.write(edad,' años tiene ',nom)// manera 2 de concaatenar
+
+let Nom='Pedro'
+let Apeliido='Paramo'
+let Saludo=Nom.concat(Apeliido)// manera 3 de concatenar
+
+document.write('<br>')
+document.write(Saludo)
---
+++ varibles/scrip.js
@@ -0,0 +1,15 @@
+//variables en javascript (tipado dinamico)
+let nombre ='Hola mundo'; //variable tipo cadenas de caracter
+let entero=12; //variable tipo entero
+let flotante=1.3; //variable tipo flotante
+let bool = true; // variable buliano
+let is_null = null;
+let persona={Nombre: 'Juan', Edad: 32}; //variable tipo objetos
+
+document.write(nombre,"<br>", //salto de lineas "<br>"
+               entero,"<br>",
+               flotante,"<br>",
+               bool,"<br>",
+               is_null,"<br>",
+               persona.Edad //variable tipo objetos
+               ); // salida del mensaje o resultado
