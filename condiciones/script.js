5509848b5f8ec21303c7c77c79c725586bb70e12
Cristian Yancis <bernardoyancis@gmail.com>
2024-08-31 15:39:50 -0600

    cambio 4.7

--- condiciones/script.js
+++ condiciones/script.js
@@ -1,8 +1,24 @@
 let v1=4
 
-if (v1=='4'){
-    document.write("son iguales")
+if (v1=='4'){ // condicion
+    document.write("son iguales"+'<br>') // resultado si se cumple
 }
 else[
-    document.write('no son iguales')
+    document.write('no son iguales'+'<br>')// resultado si no cumple
 ]
+
+switch (v1){//switch
+    case 1:
+        document.write('caso 1') // caso 1
+        break
+    case 2:
+        document.write('caso 2')// caso 2
+        break
+    case 3:
+       document.write('caso 3')// caso 3
+        break
+    
+    case 4:
+        document.write('caso 4') // caso 4
+        break
+}
