af697458b55217b1a353d9eec0c0cf48d6ae649f
Cristian Yancis <bernardoyancis@gmail.com>
2024-09-03 21:42:50 -0600

    acualización funciones en javascript

---
+++ funcion/script.js
@@ -0,0 +1,12 @@
+edadd(15)
+
+function edadd(edad){
+    if (edad>=18){
+        console.log("es mayor de edad");
+    }else if(edad<18 && edad>=14){
+        console.log('eres menor de edad');
+    }
+    else{
+        console.log('eres un niño')
+    }
+}
